2110b1916ae4f1067bb68b8ad62e92cb4578edd7
aiba-gento <8gn24gn25@gmail.com>
2026-07-21 21:00:54 +0900

    feat:WIP UDP用ドライバ作成

---
+++ robot_port/include/robot_port/linux_udp_driver.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <netinet/in.h>
+#include <stdint.h>
+
+#include "gn10_can/drivers/fdcan_driver_interface.hpp"
+
+namespace gn10_can {
+namespace drivers {
+
+class LinuxUDPDriver : public IFDCANDriver
+{
+public:
+    LinuxUDPDriver();
+    virtual ~LinuxUDPDriver();
+
+    LinuxUDPDriver(const LinuxUDPDriver&)            = delete;
+    LinuxUDPDriver& operator=(const LinuxUDPDriver&) = delete;
+
+    /**
+     * @brief ソケットの初期化とバインド
+     */
+    bool open();
+
+    /**
+     * @brief ソケットのクローズ
+     */
+    void close();
+
+    /**
+     * @brief IFDCANDriverからの継承: フレーム送信
+     */
+    bool send(const FDCANFrame& frame) override;
+
+    /**
+     * @brief IFDCANDriverからの継承: フレーム受信
+     */
+    bool receive(FDCANFrame& out_frame) override;
+
+    /**
+     * @brief WaitSet等で使用するためのファイル記述子取得
+     */
+    int get_socket_fd() const
+    {
+        return socket_fd_;
+    }
+
+private:
+    int socket_fd_{-1};
+    int on_ = 1;
+};
+
+}  // namespace drivers
+}  // namespace gn10_can
