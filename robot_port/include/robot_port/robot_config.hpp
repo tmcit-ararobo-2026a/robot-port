9aa1d92d216d232058d0098ac3b9b5115b2cf053
aiba-gento <8gn24gn25@gmail.com>
2026-09-10 08:17:17 +0900

    feat:robot configをメイン基板と一致

--- robot_port/include/robot_port/robot_config.hpp
+++ robot_port/include/robot_port/robot_config.hpp
@@ -51,25 +51,14 @@ constexpr uint8_t teleop[]    = {192, 168, 2, 2};
 struct command_t {
     // 識別ヘッダー 1byte
     uint8_t header;
-    // 足回り 12byte
-    float x_vel;        //[m/s]
-    float y_vel;        //[m/s]
-    float angular_vel;  //[rad/s]
-    // バケツ用アーム 2byte
-    uint8_t bucket_arm_hight;  //[cm]
-    bool bucket_arm_hold;
-    // ベルト直動 6byte
-    float belt_vel;  //[m/s]
-    bool belt_throw;
-    bool belt_init;
-    // エアシリンダー射出 3byte
-    bool air_launcher_for_flag;
-    bool air_launcher_for_desk_r;
-    bool air_launcher_for_desk_l;
-    // 装填処理 1byte
-    bool loading;
-    // 予備 7byte
-    uint8_t reserved[7];
+    uint8_t reserved[2];
+    bool estimated_success;           // 推定成功
+    float move_bucket_angle_yaw_rad;  // ロボット座標系における移動バケツの水平角[rad]
+    float bucket1_angle_yaw_rad;      // ロボット座標系におけるバケツ1の水平角[rad]
+    float bucket2_angle_yaw_rad;      // ロボット座標系におけるバケツ2の水平角[rad]
+    float bucket3_angle_yaw_rad;      // ロボット座標系におけるバケツ3の水平角[rad]
+    float flag_angle_yaw_rad;         // ロボット座標系における旗の水平角[rad]
+    float desk_angle_yaw_rad;         // ロボット座標系における机の水平角[rad]
 } __attribute__((__packed__));
 
 union command_u {
@@ -77,11 +66,7 @@ union command_u {
     uint8_t binary[sizeof(command_t)];  // 送信バイト配列
 } __attribute__((__packed__));
 
-static_assert(sizeof(command_t) == 32);
-
-// 後方互換用エイリアス
-using operation_t = command_t;
-using operation_u = command_u;
+static_assert(sizeof(command_t) == 28);
 
 /**
  * @brief ロボットのセンサ値などのフィードバック
@@ -100,7 +85,7 @@ struct feedback_t {
     float wheel_angular_velocity[3];  // 0:front 1:left 2:right
     float belt_launcher_velocity;     // [m/s]
     float loading_belt_angle;         // 装填機構のプーリー角度[rad]
-    float bucket_arm_hight;           // バケツアームの高さ[m]
+    float bucket_arm_height;          // バケツアームの高さ[m]
 } __attribute__((__packed__));
 
 union feedback_u {
